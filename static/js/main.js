1139ea3da706984f64d3bdce6ab2429f17a94be6
Andrew Smiley <andrew.smiley@live.com>
2014-08-21 12:17:17 -0400

    Js update

--- static/js/main.js
+++ static/js/main.js
@@ -228,12 +228,19 @@ $('.big-text, big-time, body').fadeIn("slow", function(){
 var bgCunter = 2;
 var backgroundTimer = setInterval(function(){
         var imageURL = "url(/media/background"+bgCunter+".png)";
-           $('body').css(
-     {background : imageURL,
- webkitBackgroundSize: "cover",
-         oBackgroundSize: "cover",
-         backgroundSize: "cover"
-     });
+        $('.bg-image').attr("src", imageURL);
+              var height = $(window).height();
+    var width = $(window).width();
+        $('.bg-image').attr("width", width);
+        $('.bg-image').attr("height", height);
+
+
+//           $('body').css(
+//     {background : imageURL,
+// webkitBackgroundSize: "cover",
+//         oBackgroundSize: "cover",
+//         backgroundSize: "cover"
+//     });
 //alert(bgCunter);
 
 
@@ -283,6 +290,9 @@ alert($(window).height()+" " + $(window).width());
 //    alert(screen.height.valueOf()+ " " + screen.width);
     //here's where we will dynamically set the font size and such
 //if(750 < screen.height.valueOf() <800 || 1300 < screen.width.valueOf() < 1399){
+
+//    $("body").height(height);
+//    $("body").width(width);
     if (1190 <= $(window).height() && 1800 <= $(window).width() ){
         alert("large");
         $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', url+"large/main.css") );
